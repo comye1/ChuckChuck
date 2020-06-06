b10fbb67d7d284e1f879844fa7eeaaf423f9cce3
comye1 <50735594+comye1@users.noreply.github.com>
2020-06-07 00:16:18 +0900

    fragment 1 - layout, functions

--- app/src/main/java/com/example/chuckchuck/Frag1.java
+++ app/src/main/java/com/example/chuckchuck/Frag1.java
@@ -4,19 +4,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Frag1 extends Fragment {
     private View view;
+    private TextView tv_date;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag1, container, false);
-
+        tv_date = view.findViewById(R.id.tv_date);
+        tv_date.setText(getDate());
         return view;
     }
+
+    String getDate(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("M월 d일 ");
+        String day = simpleDateFormat.format(new Date());
+        switch(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+            case 1:
+                day += "일요일";
+                break;
+            case 2:
+                day += "월요일";
+                break;
+            case 3:
+                day += "화요일";
+                break;
+            case 4:
+                day += "수요일";
+                break;
+            case 5:
+                day += "목요일";
+                break;
+            case 6:
+                day += "금요일";
+                break;
+            case 7:
+                day += "토요일";
+                break;
+
+        }
+        return day;
+    }
 }
+
+
