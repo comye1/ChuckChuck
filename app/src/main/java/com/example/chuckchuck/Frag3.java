9ceae1290c119f9be20cfefaf00b192be7279dae
comye1 <50735594+comye1@users.noreply.github.com>
2020-06-07 01:27:50 +0900

    Fragment 3 - layout

--- app/src/main/java/com/example/chuckchuck/Frag3.java
+++ app/src/main/java/com/example/chuckchuck/Frag3.java
@@ -16,13 +16,22 @@ import com.google.firebase.auth.FirebaseUser;
 public class Frag3 extends Fragment {
     private View view;
     private TextView tv_user;
+    private TextView tv_account;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3, container, false);
         tv_user = view.findViewById(R.id.tv_user);
+        tv_account = view.findViewById(R.id.tv_account);
         tv_user.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        tv_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return view;
     }
 }
