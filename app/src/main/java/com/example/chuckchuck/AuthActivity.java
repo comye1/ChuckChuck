59ddecb5e4500bda9d23c8aebb61c13c4247890b
comye1 <50735594+comye1@users.noreply.github.com>
2020-06-06 19:46:15 +0900

    firebase google로그인

---
+++ app/src/main/java/com/example/chuckchuck/AuthActivity.java
@@ -0,0 +1,109 @@
+package com.example.chuckchuck;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class AuthActivity extends AppCompatActivity {
+    private static String TAG = "AuthActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private static final int RC_SIGN_NEW = 9002;
+    private static final int RC_SIGN_EX = 9003;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton signInButton;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        signInButton = findViewById(R.id.signInButton);
+        intent = getIntent();
+
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            setResult(RC_SIGN_EX);// 로그인 되어있을 때 result
+            finish();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            }catch (ApiException e){
+
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) { //update ui code here
+        if (user != null) {
+            setResult(RC_SIGN_NEW);
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/example/chuckchuck/MainActivity.java
+++ app/src/main/java/com/example/chuckchuck/MainActivity.java
@@ -1,57 +1,96 @@
 package com.example.chuckchuck;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
     private Frag1 frag1;
     private Frag2 frag2;
     private Frag3 frag3;
+
+    private static final int REQUEST = 9999;
+    private static final int RC_SIGN_NEW = 9002;
+    private static final int RC_SIGN_EX = 9003;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST){
+
+            mAuth = FirebaseAuth.getInstance();
+            mUser = mAuth.getCurrentUser();
+
+            if(resultCode == RC_SIGN_NEW){
+                Toast.makeText(getApplicationContext(), mUser.getEmail() + "로 로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+            }else if (resultCode == RC_SIGN_EX){
+                Toast.makeText(getApplicationContext(), "로그인 상태입니다.", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                finishAffinity();
+            }
+
+            setContentView(R.layout.activity_main);
+
+
+            bottomNavigationView = findViewById(R.id.bottomNav);
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.action_home:
+                            setFrag(0);
+                            break;
+                        case R.id.action_dashboard:
+                            setFrag(1);
+                            break;
+                        case R.id.action_settings:
+                            setFrag(2);
+                            break;
+                    }
+                    return true;
+                }
+            });
+
+            frag1 = new Frag1();
+            frag2 = new Frag2();
+            frag3 = new Frag3();
+            setFrag(0); //첫 fragment
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //authActivity 통해서 result 받기
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivityForResult(intent, REQUEST);
 
-        setContentView(R.layout.activity_main);
+        ///
 
-
-        bottomNavigationView = findViewById(R.id.bottomNav);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.action_home:
-                        setFrag(0);
-                        break;
-                    case R.id.action_dashboard:
-                        setFrag(1);
-                        break;
-                    case R.id.action_settings:
-                        setFrag(2);
-                        break;
-                }
-                return true;
-            }
-        });
-
-        frag1 = new Frag1();
-        frag2 = new Frag2();
-        frag3 = new Frag3();
-        setFrag(0); //첫 fragment
 
 
     }
