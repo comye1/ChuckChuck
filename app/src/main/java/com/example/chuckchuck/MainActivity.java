24d3df2d09a36b98ad04fb0af7fb8e5b850c7585
comye1 <50735594+comye1@users.noreply.github.com>
2020-06-06 16:12:58 +0900

    fragments 연결

--- app/src/main/java/com/example/chuckchuck/MainActivity.java
+++ app/src/main/java/com/example/chuckchuck/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.chuckchuck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_home:
+                        setFrag(0);
+                        break;
+                    case R.id.action_dashboard:
+                        setFrag(1);
+                        break;
+                    case R.id.action_settings:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        setFrag(0); //첫 fragment
+
+
+    }
+
+
+    //fragment 교체
+    private void setFrag(int n){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch(n){
+            case 0:
+                fragmentTransaction.replace(R.id.mainFrame, frag1);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentTransaction.replace(R.id.mainFrame, frag2);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentTransaction.replace(R.id.mainFrame, frag3);
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
