f43aabc6b0d649d34f81dc09a202a7e060651e31
comye1 <50735594+comye1@users.noreply.github.com>
2020-06-09 11:26:50 +0900

    TimeTable 클래스 정의

--- app/src/main/java/com/example/chuckchuck/Frag1.java
+++ app/src/main/java/com/example/chuckchuck/Frag1.java
@@ -8,12 +8,16 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.wefika.flowlayout.FlowLayout;
 
 import org.w3c.dom.Text;
@@ -21,12 +25,17 @@ import org.w3c.dom.Text;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.zip.Inflater;
 
 public class Frag1 extends Fragment {
     private View view;
     private View keywordView, record1;
     private TextView tv_date;
     private LinearLayout linearScroll;
+    private LayoutInflater linflater;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private static int dayToday;
 
     @Nullable
     @Override
@@ -36,22 +45,20 @@ public class Frag1 extends Fragment {
         tv_date.setText(getDate());
         linearScroll = view.findViewById(R.id.linear_scroll);
 
-        //todo 메소드 만들기 (요일 해당 과목 찾아서 아래 뷰 만들고 추가하기 & 클릭리스너
-        //todo firebase에 초기화 (keyword: 키워드)
-        record1 = inflater.inflate(R.layout.frag_todayrecord, container, false);
-        FlowLayout flowLayout = (FlowLayout)record1.findViewById(R.id.flowLayout);
-        TextView textView = KeywordTextView();
-        textView.setText("sample keyword");
-        flowLayout.addView(textView);
-        linearScroll.addView(record1);
-        /////
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        createTodayRecords();
+
+
         return view;
     }
 
     String getDate(){
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("M월 d일 ");
         String day = simpleDateFormat.format(new Date());
-        switch(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+        dayToday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK); //오늘 요일 저장
+        switch(dayToday){
             case 1:
                 day += "일요일";
                 break;
@@ -78,6 +85,23 @@ public class Frag1 extends Fragment {
         return day;
     }
 
+    private void createTodayRecords(){
+
+
+        linflater = getLayoutInflater();
+        //todo 메소드 만들기 (요일 해당 과목 찾아서 아래 뷰 만들고 추가하기 & 클릭리스너
+        //todo firebase에 초기화 (keyword: 키워드)
+        record1 = linflater.inflate(R.layout.frag_todayrecord, null);
+        FlowLayout flowLayout = (FlowLayout)record1.findViewById(R.id.flowLayout);
+        // flowLayout 전달
+        TextView textView = KeywordTextView();
+        textView.setText("sample keyword");
+        textView.setOnClickListener(onclick);
+        flowLayout.addView(textView);
+        linearScroll.addView(record1);
+        /////
+    }
+
     private TextView KeywordTextView(){
         TextView textView = new TextView(getContext());
         FlowLayout.LayoutParams params = new FlowLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -86,20 +110,18 @@ public class Frag1 extends Fragment {
         textView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.round_border));
         return textView;
     }
+
+    View.OnClickListener onclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView tv = (TextView)v;
+            String sName = tv.getText().toString();
+            Toast.makeText(getContext(),sName + " clicked", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
 }
 
-class KeywordTextView{
-//<TextView xmlns:android="http://schemas.android.com/apk/res/android"
-//        android:layout_width="wrap_content" android:layout_height="wrap_content"
-//        android:layout_margin="5dp" android:background="@drawable/round_border"
-//        android:padding="5dp"
-//        android:text="sample"/>
-    private LinearLayout.LayoutParams params;
-    private TextView textView;
-
-    KeywordTextView(){
-
-    }
-}
 
 
